b55972ea6ec62ed809cbe572f5b36cd0b50ed4d0
sveloudis <115147614+sveloudis@users.noreply.github.com>
2022-10-18 16:17:22 +0300

    Add files via upload

---
+++ checkform.js
@@ -0,0 +1,20 @@
+// Edit the function below to work exactly according to the requirement below
+
+function checkform()
+{
+
+/* You need to check that the value entered for the year field is blank (empty), and if it is, issue an alert
+
+/* You need to check that the value entered for the fullname field is blank (empty), and if it is, issue an alert
+
+/* You need to check that the value entered for the fullname field contains more than two words, and if it does, issue an alert
+
+if any of the above violated, the function checkform() returns false, otherwise true.
+
+*/
+
+
+}
+
+
+
