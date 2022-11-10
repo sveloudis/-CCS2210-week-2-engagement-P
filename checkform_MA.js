ce8989a410a9031ae5c4acfeee4215c0603e5ba9
sveloudis <115147614+sveloudis@users.noreply.github.com>
2022-11-10 20:44:45 +0200

    Add files via upload

---
+++ checkform_MA.js
@@ -0,0 +1,32 @@
+// 
+/* Edit the function below such that it checks whether:
+1. The year field is blank (empty), and if it is, issue an alert
+2. The fullname field contains exactly two words, and if it doesn't, issue an alert
+If the year field is empty or if the fullname field does not contain exactly two words, the function checkform() returns false, otherwise true.
+*/
+function checkform()
+{
+    let year = document.getElementById("year").value;
+    let fullName = document.getElementById("fullname").value;
+    const nameArray = fullName.split(" ");
+    console.log(year);
+    console.log(fullName);
+    console.log(nameArray);
+    
+    if (year == "") {
+        alert("Fill in the year field!");
+        return false;
+    }
+    else if (fullName == "") {
+        alert("Fill in the name field!");
+        return false;
+    }
+    else if (nameArray.length != 2) {
+        alert("Inappropriate name!");
+        return false;
+    }
+    else return true;
+
+}
+
+
